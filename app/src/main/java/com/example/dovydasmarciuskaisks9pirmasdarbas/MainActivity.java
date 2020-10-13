82f2170eba09e443948cfd00bc202ef995c8916e
Gimze <dmarciuska@gmail.com>
2020-10-13 19:57:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dovydasmarciuskaisks9pirmasdarbas/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dovydasmarciuskaisks9pirmasdarbas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
