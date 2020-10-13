5c04e1d3b8a4c5ded5427704b947017d5c162c2b
Gimze <dmarciuska@gmail.com>
2020-10-13 21:51:37 +0300

    Trecias commitas

--- app/src/main/java/com/example/dovydasmarciuskaisks9pirmasdarbas/MainActivity.java
+++ app/src/main/java/com/example/dovydasmarciuskaisks9pirmasdarbas/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dovydasmarciuskaisks9pirmasdarbas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,14 +14,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button;
+        Button button2;
         TextView txt;
         button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
         txt = (TextView) findViewById(R.id.textView);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
                 txt.setText("Dovydas Marciuska");
+
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                txt.setTextColor(Color.parseColor("#00ff15"));
 
             }
         });
